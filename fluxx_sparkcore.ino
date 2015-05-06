76d76858ce85c166959ca753a967b95788137d62
Jonathan Eng <jwsyeng@gmail.com>
2015-05-05 21:46:13 -0700

    cleaned up indents

--- fluxx_sparkcore.ino
+++ fluxx_sparkcore.ino
@@ -56,21 +56,21 @@ void setup() {
 }
 
 int powrrup(String poww){
- strip.show();
- powerUp(strip.Color(0, 0, 255), 50);
- int powww = (poww.charAt(1) - '0') - 1;
- if (powww < 0 || powww > 1) return -1;
- power ++ ;
+   strip.show();
+   powerUp(strip.Color(0, 0, 255), 50);
+   int powww = (poww.charAt(1) - '0') - 1;
+   if (powww < 0 || powww > 1) return -1;
+   power ++ ;
 }
 
 
 void loop() {
 
-  // Get current button state.
-  bool newState = digitalRead(BUTTON);
-  bool mswitch = digitalRead(MAGNET_SWITCH);
-  // boolean to indicate whether fluxx is in charging mode
-  bool powrr = false;
+    // Get current button state.
+    bool newState = digitalRead(BUTTON);
+    bool mswitch = digitalRead(MAGNET_SWITCH);
+    // boolean to indicate whether fluxx is in charging mode
+    bool powrr = false;
 
    //Code to power up
    if (mswitch == LOW && oldState == HIGH) {
@@ -80,117 +80,106 @@ void loop() {
         // Check if button is still low after debounce.
         mswitch = digitalRead(MAGNET_SWITCH);
         if (mswitch == LOW) {
-          digitalWrite(ledPin, HIGH);
-          showType++;
-          power++;
-          if (showType > 3) {
-            showType = 1;
-          }
-          strip.show();
-          powerUp(strip.Color(0, 0, 255), 50);
+            digitalWrite(ledPin, HIGH);
+            showType++;
+            power++;
+            if (showType > 3) {
+                showType = 1;
+            }
+            strip.show();
+            powerUp(strip.Color(0, 0, 255), 50);
         }
-      }
-
-  // Check if state changed from high to low (button press).
-  if (newState == LOW && oldState == HIGH) {
-
-    // Short delay to debounce button.
-    delay(20);
-    // Check if button is still low after debounce.
-    newState = digitalRead(BUTTON);
-    if (newState == LOW) {
-      showType++;
-      bpress++;
-      if (bpress % 5 == 0) {
-        hits++;
-      }
-      if (showType > 3) {
-        showType = 1;
-      }
-      strip.show();
-      startShow(showType);
     }
-  }
 
+    // Check if state changed from high to low (button press).
+    if (newState == LOW && oldState == HIGH) {
+        // Short delay to debounce button.
+        delay(20);
+        // Check if button is still low after debounce.
+        newState = digitalRead(BUTTON);
+        if (newState == LOW) {
+            showType++;
+            bpress++;
+            if (bpress % 5 == 0) {
+                hits++;
+            }
+            if (showType > 3) {
+                showType = 1;
+            }
+            strip.show();
+            startShow(showType);
+        }
+    }
 
-  // Set the last button state to the old state.
-  if (powrr){
-    oldState = mswitch;
-    powrr = false;
+    // Set the last button state to the old state.
+    if (powrr){
+        oldState = mswitch;
+        powrr = false;
     } else {
-      oldState = newState;
+        oldState = newState;
     }
+}
 
-  }
 
-  void startShow(int i) {
+void startShow(int i) {
     strip.setBrightness(100);
     switch (i) {
-    case 0: colorWipe(strip.Color(0, 0, 0), 50);    // Black/off
-    wtv020sd16p.playVoice(1);
-    break;
-    case 1: colorWipe(strip.Color(255, 0, 0), 50); //red
-    wtv020sd16p.playVoice(2);
-    break;
-    case 2: colorWipe(strip.Color(0, 255, 0), 50);  // Green
-    wtv020sd16p.playVoice(3);
-    break;
-    case 3: colorWipe(strip.Color(0, 0, 255), 50);  // Blue
-    wtv020sd16p.playVoice(4);
-    break;
-  }
-  for (int blinkloop = 0; blinkloop < 3; blinkloop++){
-   digitalWrite(ledPin, HIGH);
-       delay(500);               // wait for a second
-       digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-       delay(500);
-     }
-   }
+        case 0: colorWipe(strip.Color(0, 0, 0), 50);    // Black/off
+        wtv020sd16p.playVoice(1);
+        break;
+        case 1: colorWipe(strip.Color(255, 0, 0), 50); //red
+        wtv020sd16p.playVoice(2);
+        break;
+        case 2: colorWipe(strip.Color(0, 255, 0), 50);  // Green
+        wtv020sd16p.playVoice(3);
+        break;
+        case 3: colorWipe(strip.Color(0, 0, 255), 50);  // Blue
+        wtv020sd16p.playVoice(4);
+        break;
+    }
+}
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
-  for (uint16_t i = 0; i < (power - hits); i++) {
-    strip.setPixelColor(i, c);
-    delay(wait);
-    strip.show();
-  }
+    for (uint16_t i = 0; i < (power - hits); i++) {
+        strip.setPixelColor(i, c);
+        delay(wait);
+        strip.show();
+    }
 
-  for (uint16_t i = (power - hits); i < 16; i++) {
-    strip.setPixelColor(i, 0);
-    delay(wait);
-    strip.show();
-
-  }
+    for (uint16_t i = (power - hits); i < 16; i++) {
+        strip.setPixelColor(i, 0);
+        delay(wait);
+        strip.show();
+    }
 }
 
 //Lights up neopixel incrementally as nurse/other kid helps power up
 void powerUp(uint32_t c, uint8_t wait) {
+    for (uint16_t i = 0; i < (power); i++) {
+        strip.setPixelColor(i, c);
+        delay(wait);
+        strip.show();
+    }
 
-  for (uint16_t i = 0; i < (power); i++) {
-    strip.setPixelColor(i, c);
-    delay(wait);
-    strip.show();
-  }
-
-  for (uint16_t i = power; i < 16; i++) {
-    strip.setPixelColor(i, 0);
-    delay(wait);
-    strip.show();
-
-  }
+    for (uint16_t i = power; i < 16; i++) {
+        strip.setPixelColor(i, 0);
+        delay(wait);
+        strip.show();
+    }
 }
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    WheelPos = 255 - WheelPos;
+    if (WheelPos < 85) {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
     } else if (WheelPos < 170) {
-      WheelPos -= 85;
-      return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-      } else {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    } else {
         WheelPos -= 170;
         return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-      }
     }
+}
